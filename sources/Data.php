2049c26be8576a69f48474c959d6689783fc561b
MenyongMenying <menyongmenying.main@gmail.com>
2025-06-04 15:09:57 +0700

    Menambahkan class utama(Data).

---
+++ sources/Data.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MenyongMenying\PHP\MenyongMenyingLibrary\Data;
+
+
+/**
+ * @author MenyongMenying <menyongmenying.main@email.com>
+ * 
+ * @version 0.0.1
+ * 
+ * @since 2025-06-04
+ */
+final class Data
+{
+}
